9c76868cb42980d2bf41dc227d5b13319808890b
SewTotalEye <sewtotaleyegames@gmail.com>
2025-07-22 22:21:24 -0700

    updated background

--- the-moth-inside/scripts/result.gd
+++ the-moth-inside/scripts/result.gd
@@ -9,50 +9,50 @@ extends Control
 var traits_to_display := {}
 
 func _ready():
-    restart_button.pressed.connect(_on_restart_pressed)
-    
+	restart_button.pressed.connect(_on_restart_pressed)
+	
    
-    await get_tree().process_frame
+	await get_tree().process_frame
 
-    if traits_to_display.size() > 0:
-        display_traits()
+	if traits_to_display.size() > 0:
+		display_traits()
 
 
 func set_traits(collected_traits: Dictionary) -> void:
-    traits_to_display = collected_traits
+	traits_to_display = collected_traits
 
 func display_traits() -> void:
-    print("Received traits:", traits_to_display)
+	print("Received traits:", traits_to_display)
 
    
-    var trait_text := ""
-    for t in traits_to_display.keys():
-        trait_text += "• %s\n" % t
-    trait_list.text = trait_text.strip_edges()
+	var trait_text := ""
+	for t in traits_to_display.keys():
+		trait_text += "• %s\n" % t
+	trait_list.text = trait_text.strip_edges()
 
    
-    name_label.text = generate_name(traits_to_display)
-    blurb_label.text = generate_blurb(traits_to_display)
+	name_label.text = generate_name(traits_to_display)
+	blurb_label.text = generate_blurb(traits_to_display)
 
 func generate_name(traits: Dictionary) -> String:
-    if traits.has("glow") and traits.has("bright_palette"):
-        return "Sunwing"
-    elif traits.has("frayed_edges") and traits.has("pastel_palette"):
-        return "Paperflor"
-    elif traits.has("neutral_palette"):
-        return "Neuluna"
-    else:
-        return "Unnamed Moth"
+	if traits.has("glow") and traits.has("bright_palette"):
+		return "Sunwing"
+	elif traits.has("frayed_edges") and traits.has("pastel_palette"):
+		return "Paperflor"
+	elif traits.has("neutral_palette"):
+		return "Neuluna"
+	else:
+		return "Unnamed Moth"
 
 func generate_blurb(traits: Dictionary) -> String:
-    if traits.has("fuzzy"):
-        return "This moth spends long days flying higher than average. It is said that it wishes to be a bird."
-    elif traits.has("torn_wing"):
-        return "Even damaged wings can carry great strength. This one proves it."
-    elif traits.has("bold_pattern"):
-        return "This moth finds strengh in a bold world. It is said it gets bolder with age."
-    else:
-        return "A mysterious presence, fluttering between what is seen and unseen."
+	if traits.has("fuzzy"):
+		return "This moth spends long days flying higher than average. It is said that it wishes to be a bird."
+	elif traits.has("torn_wing"):
+		return "Even damaged wings can carry great strength. This one proves it."
+	elif traits.has("bold_pattern"):
+		return "This moth finds strengh in a bold world. It is said it gets bolder with age."
+	else:
+		return "A mysterious presence, fluttering between what is seen and unseen."
 
 func _on_restart_pressed():
-    get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
